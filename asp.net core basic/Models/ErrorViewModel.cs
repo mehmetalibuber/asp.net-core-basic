98627e1c3d5aa89de509df0d8a7b31de8328df89
mehmetalibuber <mehmetalibuber6@gmail.com>
2023-02-24 11:14:09 +0300

    Proje dosyası ekle.

---
+++ asp.net core basic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp.net_core_basic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
